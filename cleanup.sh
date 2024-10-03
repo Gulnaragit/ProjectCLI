095679cd34c4c1274f16bfab02d8af7490f00252
gulnara <gulnarachyntaeva@gmail.com>
2024-10-03 06:24:18 +0000

    Fixed Nat Gateway, added another route table and associated it with subnet2

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,31 @@
 #!/bin/bash 
 
 
-# Delete EC2 Instance
-aws ec2 terminate-instances --instance-ids 
+# Find EC2 Instance Ids for Public and Private Instances
+public_ec2=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId[]' --filter 'Name=tag:Name, Values=Public-CLI-Ec2' --output text)
+private_ec2=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId[]' --filter 'Name=tag:Name, Values=Private-CLI-Ec2' --output text)
+
+
+# Terminate EC2 Instance
+aws ec2 terminate-instances --instance-ids $public_ec2 
+aws ec2 terminate-instances --instance-ids $private_ec2
+
+echo "Will continue deleting after sleep of 60seconds"
+sleep 60 
+
+# Find NAT gateway Id 
+natgw_id=$(aws ec2 describe-nat-gateways --filter 'Name=tag:Name, Values=custom-natgw)
+
+
+
+# --query' 'NatGateways[*].NatGatewayId[]' --output text)
+# Find Allocation Id or Elasric IP
+#aws ec2 describe-addresses --filters 'Name=tag:Name, Values=elastic-ip' --query 'addresses[*] allocationId[]' --output text 
+
+
+
+# Find VPC Id need to be deleted
+#aws ec2 describe-vpcs --query 'Vpcs[?contains(Tags[?Key==`Name`].Value[], `MyVPC`) == `true`].[VpcId]'
 
 
 # Delete VPC, it will aslo delete subnets, igw, route tables & security group
--- test.sh
+++ test.sh
@@ -27,16 +27,19 @@ sub3=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr3 --avail
 igw_id=$(aws ec2 create-internet-gateway --region $region --tag-specifications 'ResourceType=internet-gateway, Tags=[{Key=Name,Value=Custom-igw}]' --query InternetGateway.InternetGatewayId --output text)
 
 # Attach Internet gateway to VPC
-aws ec2 attach-internet-gateway --vpc-id $vpc_id --internet-gateway-id $igw_id --region $region --query InternetGateway.InternetGatewayId --output text
+aws ec2 attach-internet-gateway --vpc-id $vpc_id --internet-gateway-id $igw_id --region $region --query InternetGateway.InternetGatewayId --output text > /dev/null
 
-# Create Route table and assign to Kaizen VPC
-rtb_id=$(aws ec2 create-route-table --vpc-id $vpc_id --tag-specifications 'ResourceType=route-table, Tags=[{Key=Name,Value=Custom-rtb}]' --query RouteTable.RouteTableId --output text)
+# Create Public route table and assign to Kaizen VPC
+public_rtb_id=$(aws ec2 create-route-table --vpc-id $vpc_id --tag-specifications 'ResourceType=route-table, Tags=[{Key=Name,Value=Custom-rtb}]' --query RouteTable.RouteTableId --output text)
+
+# Create Private route table and assign to Kaizen VPC
+private_rtb_id=$(aws ec2 create-route-table --vpc-id $vpc_id --tag-specifications 'ResourceType=route-table, Tags=[{Key=Name,Value=Custom-rtb}]' --query RouteTable.RouteTableId --output text)
 
 # Attach route table to Internet Gateway
-aws ec2 create-route --route-table-id $rtb_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id > /dev/null 
+aws ec2 create-route --route-table-id $public_rtb_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id > /dev/null 
 
 # Associate public subnet to route table
-aws ec2 associate-route-table --subnet-id $sub1 --route-table-id $rtb_id > /dev/null
+aws ec2 associate-route-table --subnet-id $sub1 --route-table-id $public_rtb_id > /dev/null
 
 # Create security group
 sg_id=$(aws ec2 create-security-group --group-name EC2SecurityGroup --description "Project Security Group" --vpc-id $vpc_id --tag-specifications 'ResourceType=security-group, Tags=[{Key=Name,Value=Custom-sg}]' --query GroupId --output text)
@@ -50,15 +53,33 @@ aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port
 # Allocate Elastic IP
 elastic_ip=$(aws ec2 allocate-address --domain $vpc_id --tag-specifications 'ResourceType=elastic-ip, Tags=[{Key=Name,Value=elastic-ip}]' --query AllocationId --output text)
 
-# Create NAT gateway associate it with above Elastic Ip and subnet2
-aws ec2 create-nat-gateway --subnet-id $sub2 --allocation-id $elastic_ip > /dev/null 
+# Create NAT gateway associate it with above Elastic Ip and Public subnet1
+nat_gtw_id=$(aws ec2 create-nat-gateway --subnet-id $sub1 --allocation-id $elastic_ip --query NatGateway.NatGatewayId --output text)
+
+echo "Stoping the execution of the code for 60seconds for the NAT gateway to become available"
+sleep 60
+echo "Now continuing the execution of the code..."
+
+# Tag NAT Gateway
+aws ec2 create-tags --resources $nat_gtw_id --tags Key=Name,Value=custom-natgw
+
+# Attach route table to NAT Gateway
+aws ec2 create-route --route-table-id $private_rtb_id --destination-cidr-block 0.0.0.0/0 --nat-gateway-id $nat_gtw_id > /dev/null 
+
+# Associate private subnet to route table
+aws ec2 associate-route-table --subnet-id $sub2 --route-table-id $private_rtb_id > /dev/null
+
+# Import public key of my local machine to AWS console
+aws ec2 import-key-pair --key-name "ProjectKeyPair" --public-key-material fileb://~/.ssh/id_rsa.pub > /dev/null
 
 # Create EC2 instance in public Subnet1
-ec2_id_1=$(aws ec2 run-instances --image-id ami-0ebfd941bbafe70c6 --count 1 --instance-type t2.micro  --security-group-ids $sg_id --subnet-id $sub1 --associate-public-ip-address --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Public-CLI-Ec2}]' --query Instances[0].InstanceId --output text)
+ec2_id_1=$(aws ec2 run-instances --image-id ami-0ebfd941bbafe70c6 --count 1 --instance-type t2.micro  --security-group-ids $sg_id --subnet-id $sub1 --associate-public-ip-address --key-name 'ProjectKeyPair' --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Public-CLI-Ec2}]' --query Instances[0].InstanceId --output text)
+
+# Get the Public IP of the public Instance
+public_ip=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress[]' --filter 'Name=tag:Name, Values=Public-CLI-Ec2' --output text)
+
+echo Public Ip of the Instance: $public_ip
 
 # Create EC2 instance in private Subnet2
 ec2_id_2=$(aws ec2 run-instances --image-id ami-0ebfd941bbafe70c6 --count 1 --instance-type t2.micro  --security-group-ids $sg_id --subnet-id $sub2 --associate-public-ip-address --tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Private-CLI-Ec2}]' --query Instances[0].InstanceId --output text)
 
-
-echo Public subnet EC2 Instance ID: $ec2_id_1 
-echo Private subnet EC2 Instance ID: $ec2_id_2 
